47a6c18e4223a75ad0e214f8442eb6c3d9d30f3d
neozxin <neozxin@hotmail.com>
2024-11-15 17:29:29 +0800

    Update inst-docker.sh

--- inst-docker.sh
+++ inst-docker.sh
@@ -29,14 +29,14 @@ inst_docker_via_apt() {
 
 inst_docker_via_deb() {
   # Install Docker via .deb
-  INST_TMP_DIR="./temp$(date +%s)" \
+  INST_TMP_DIR="./temp$(date +%s)"
   INST_OS_ID="$(. /etc/os-release && echo "$ID")" # "ubuntu"
   INST_OS_VERSION_ID="$(. /etc/os-release && echo "$VERSION_ID")" # "22.04"
   INST_OS_CODENAME="$(. /etc/os-release && echo "$VERSION_CODENAME")" # "jammy"
   INST_OS_ARCH="$(dpkg --print-architecture)" # "amd64"
   INST_OS_IVCA="${INST_OS_ID}.${INST_OS_VERSION_ID}~${INST_OS_CODENAME}_${INST_OS_ARCH}"
-  INST_PKG_NAMES="containerd.io_1.7.22-1_${INST_OS_ARCH}.deb docker-ce_27.2.1-1~${INST_OS_IVCA}.deb docker-ce-cli_27.2.1-1~${INST_OS_IVCA}.deb docker-buildx-plugin_0.16.2-1~${INST_OS_IVCA}.deb docker-compo>
-  INST_FROM_URL="https://download.docker.com/linux/ubuntu/dists/${INST_OS_CODENAME}/pool/stable/${INST_OS_ARCH}/" \
+  INST_PKG_NAMES="containerd.io_1.7.22-1_${INST_OS_ARCH}.deb docker-ce_27.2.1-1~${INST_OS_IVCA}.deb docker-ce-cli_27.2.1-1~${INST_OS_IVCA}.deb docker-buildx-plugin_0.16.2-1~${INST_OS_IVCA}.deb docker-compose-plugin_2.29.2-1~${INST_OS_IVCA}.deb"
+  INST_FROM_URL="https://download.docker.com/linux/ubuntu/dists/${INST_OS_CODENAME}/pool/stable/${INST_OS_ARCH}/"
   sudo apt update
   wget -N $(for pkg_name in ${INST_PKG_NAMES}; do printf " ${INST_FROM_URL}${pkg_name}"; done) -P "${INST_TMP_DIR}/"
   test $(ls "${INST_TMP_DIR}" | wc -l) -eq "$(echo "${INST_PKG_NAMES}" | wc -w)"
@@ -51,7 +51,30 @@ main() {
     inst_docker_via_apt "$@"
   elif [ "inst_docker_via_deb" = "$1" ]; then
     inst_docker_via_deb "$@"
+  else
+    echo '[zx] sh unknown args...'
+    exit 1
   fi
+
+cat <<EOF
+sudo nano /etc/docker/daemon.json
+{
+  "registry-mirrors": [
+    "https://docker.rainbond.cc",
+    "https://docker.m.daocloud.io",
+    "https://noohub.ru",
+    "https://huecker.io",
+    "https://dockerhub.timeweb.cloud",
+    "https://dockerproxy.com",
+    "https://docker.nju.edu.cn",
+    "https://docker.mirrors.sjtug.sjtu.edu.cn",
+    "https://ung2thfc.mirror.aliyuncs.com",
+    "https://registry.docker-cn.com",
+    "http://hub-mirror.c.163.com",
+    "https://docker.mirrors.ustc.edu.cn"
+  ]
+}
+EOF
 }
 
 main "$@"
